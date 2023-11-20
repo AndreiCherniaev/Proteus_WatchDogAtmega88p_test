0bf0db001bf19b82cfafddd2f1338a0a65e7c4fc
AndreiCherniaev <glscene2010@gmail.com>
2023-11-21 00:56:48 +0900

    Update main.c

--- code/code/main.c
+++ code/code/main.c
@@ -12,7 +12,7 @@ int main(void){
 	wdt_enable(WDTO_2S);
 	/* Reset WDT */
 	//wdt_enable(WDTO_2S); //1)if you just enable again then watchdog will disabled forever
-	//asm( "WDR "); //2)if you try reset watchdog once then watchdog will disabled forever
+	//asm( "WDR"); //2)if you try reset watchdog once then watchdog will disabled forever
 	//wdt_reset(); //3)if you try reset watchdog once then watchdog will disabled forever
 	//4)even if you enable watchdog in fuse in Proteus you can disable it using 1) or 2) or 3)
 	while(1){
